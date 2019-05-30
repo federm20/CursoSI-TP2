3a2777b60ec0ffb90493bbad8005fceb354fc8be
Federico Mione <fedemione@gmail.com>
2019-05-30 16:06:57 -0300

    generador de imagenes

--- act2.py
+++ act2.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot
 
 # obtiene las imagenes y su clasificacion
-bunch_dataset = skdataset.load_files("shapes/Filtered/")
+bunch_dataset = skdataset.load_files("shapes2/")
 
 # inicializa array de tensores
 bunch_dataset["image_tensor"] = []
@@ -17,6 +17,11 @@ with tf.Session() as sess:
         bunch_dataset["image_tensor"].append(sess.run(tf.image.decode_png(image_png, channels=1)) / 255.0)
         print('Loading image ', len(bunch_dataset["image_tensor"]))
 
+# model = tf.keras.Sequential()
+# layer = model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+# print(layer.predict(bunch_dataset["image_tensor"][0]))
+
+
 # separa datos para entrenamiento y testeo
 X_train, X_test, y_train, y_test = train_test_split(bunch_dataset['image_tensor'], bunch_dataset['target'],
                                                     test_size=0.25)
@@ -25,8 +30,10 @@ X_train, X_test, y_train, y_test = train_test_split(bunch_dataset['image_tensor'
 model = tf.keras.Sequential()
 
 # arquitectura ConvNet
-# model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
 # model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
 # model.add(tf.keras.layers.MaxPooling2D((2, 2)))
 
@@ -37,18 +44,18 @@ model.add(tf.keras.layers.Flatten(input_shape=(28, 28, 1)))
 # model.add(tf.keras.layers.Dense(64, activation='sigmoid'))
 # model.add(tf.keras.layers.Dense(32, activation='sigmoid'))
 
-# model.add(tf.keras.layers.Dense(1024, activation='relu'))
+model.add(tf.keras.layers.Dense(1024, activation='relu'))
 # model.add(tf.keras.layers.Dropout(0.2))
-# model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dense(512, activation='relu'))
 # model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.Dense(256, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.2))
+# model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.2))
-model.add(tf.keras.layers.Dense(64, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.2))
-model.add(tf.keras.layers.Dense(32, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.2))
+# model.add(tf.keras.layers.Dropout(0.2))
+# model.add(tf.keras.layers.Dense(64, activation='relu'))
+# model.add(tf.keras.layers.Dropout(0.2))
+# model.add(tf.keras.layers.Dense(32, activation='relu'))
+# model.add(tf.keras.layers.Dropout(0.2))
 
 
 # probabilidad de cada clase
@@ -58,7 +65,7 @@ model.add(tf.keras.layers.Dense(5, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Entrena el modelo
-history = model.fit(np.array(X_train), y_train, validation_data=(np.array(X_test), y_test), epochs=400)
+history = model.fit(np.array(X_train), y_train, validation_data=(np.array(X_test), y_test), epochs=100)
 
 print('-----------------------------------------------------------')
 print(model.summary())
